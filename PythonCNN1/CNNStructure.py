4103e340efab604a96fc8db868b1fd621e7d8800
Karl Reinig <Karl.Reinig@gmail.com>
2019-02-02 11:39:19 -0700

    Finished first round of CNNStructure without the abilit to read in the weights.

--- PythonCNN1/CNNStructure.py
+++ PythonCNN1/CNNStructure.py
@@ -5,7 +5,8 @@ import random
 class CNNStructure(object):
     def __init__(self, ftOrL, w = 0, b = 0):
         print("Type of input = ",type(ftOrL))
-        
+        self.layerNodes = [] 
+        self.weights = []
         if type(ftOrL) is str:
             print("Choosing constructor from file ", ftOrL)
             self.constructorFromFile(ftOrL)
@@ -26,14 +27,13 @@ class CNNStructure(object):
         Construct weighting matrices to take the input nodes and calculate the output nodes. The length of the list
         (minus one) gives the number of matrices. Considering consecutive pairs from the list, the first number
         determines the number of columns, the second number determines the number of rows. Going to construct 
-        a list of numpy arrays. Note, each matrix gets one extra row of 0s ending in a 1.
+        a list of numpy arrays. Note, each matrix gets a row of 0s ending in a 1 for the last row.
         """
         numLayers = len(List)
         random.seed(0)
-        self.weights = []
-        print("numLayers ",numLayers)
+
         for layerCount in range(numLayers-1):
-            numRows = List[layerCount+1]+1
+            numRows = List[layerCount+1]
             numCols = List[layerCount]
             tWeights = np.array([[0 for col in range(numCols)] for row in range(numRows)],dtype=float) 
             if(w != 0 or b !=0):    #w and b = zero is a special case to construct a zero filled matrix
@@ -43,19 +43,89 @@ class CNNStructure(object):
 
             tWeights[numRows-1,numCols-1] = 1.
             self.weights.append(tWeights)
-        print(self.weights)
+
         self.addLayerNodes(List)
 
 # Make space for the layer nodes.
     def addLayerNodes(self,List):
         numLayers = len(List)
-        self.layerNodes = []
+
         for layerCount in range(numLayers):
-            numRows = List[layerCount]+1
-            tempLayer = np.array([0 for col in range(numRows)],dtype=float)
+            numRows = List[layerCount]
+            tempLayer = np.array([0 for col in range(numRows)],dtype=float).reshape(numRows,1)
             tempLayer[numRows-1] = 1.
             self.layerNodes.append(tempLayer)
-
+        print("Show all the layerNodes")
         print(self.layerNodes)
 
-        
+    def updateLayerNodes(self,inputLayer):
+        self.layerNodes[0] = inputLayer
+        print("len(self.weights) ",len(self.weights))
+        for layerCount in range (len(self.weights)):
+            if layerCount is 0:
+                tempLayer = inputLayer
+            
+            print("The matrix")
+            print(self.weights[layerCount])
+            print("The vector")
+            print(tempLayer)
+            
+            self.layerNodes[layerCount+1] = np.dot(self.weights[layerCount],tempLayer)
+            print("After matrix mult")
+            print(self.layerNodes[layerCount+1])
+#Apply sigma
+           
+            for rowCount in range(len(self.layerNodes[layerCount+1])):
+                if self.layerNodes[layerCount+1][rowCount]< 0:
+                    self.layerNodes[layerCount+1][rowCount] = 0
+            
+            tempLayer = self.layerNodes[layerCount+1]
+ #           print(tempLayer)
+
+    def makeGradPass(self,tempGradStruct,desired):
+        print("len(self.weights)",len(self.weights))
+        for layerCount in range(len(self.weights),0,-1):
+            print("layerCount ",layerCount)
+            partRelu = np.dot(self.weights[layerCount-1],self.layerNodes[layerCount-1])
+            if layerCount == len(self.weights):
+                pCpA = 2.*(self.layerNodes[layerCount] - desired)
+                print("pCpA")
+                print(pCpA)
+
+            for rowCount in range(len(self.weights[layerCount - 1])-1):
+
+                if partRelu[rowCount] < 0.: 
+	                partRelu[rowCount] = 0.
+                
+                else: 
+	                partRelu[rowCount] = 1.
+                
+                print("len(self.weights[layerCount-1][0]) ",len(self.weights[layerCount-1][0]))
+                for colCount in range(len(self.weights[layerCount-1][0])-1):
+                    print("colCount",colCount)
+                    tempGradStruct.weights[layerCount - 1][rowCount][colCount] =self.layerNodes[layerCount - 1][colCount] * partRelu[rowCount] *pCpA[rowCount];
+			
+#Each row also has a bias term at the end of the row.
+                tempGradStruct.weights[layerCount - 1][rowCount][len(self.weights[layerCount - 1][0])-1] =partRelu[rowCount] * pCpA[rowCount];
+
+            temppCpA = np.array
+            temppCpA = np.zeros(([len(self.weights[layerCount - 1][0])-1,1]),dtype=np.float)
+    #		//Calculate the pCpA vector for the next round.
+            for colCount in range(len(self.weights[layerCount - 1][0])-1): 
+	                tempSum = 0.;
+	                for rowCount in range(len(self.weights[layerCount - 1]) - 1):
+		                tempSum += self.weights[layerCount - 1][rowCount][colCount]*partRelu[rowCount]*pCpA[rowCount];
+				
+	                temppCpA[colCount] =tempSum
+            pCpA =temppCpA;
+
+    def calcCost(self,input,desired):
+        self.updateLayerNodes(input)
+#The cost only depends on the last layer's nodes. And there is no addition term added to the vector.
+        costSum = 0.
+        numLayers = len(self.layerNodes)
+        for iCnt in range(len(desired)-1): 	# Cut down by one because desired has the extra 1.
+										    #It doesn't really matte r since both have 1 at the end.
+            costSum += (self.layerNodes[numLayers-1][iCnt] - desired[iCnt])**2
+	
+        return(costSum);
--- PythonCNN1/PythonCNN1.py
+++ PythonCNN1/PythonCNN1.py
@@ -6,25 +6,43 @@ import numpy as np
 from imagesAndLabels import imagesAndLabels
 from CNNStructure import CNNStructure
 
-trainingData = imagesAndLabels('training','./data/')
+#trainingData = imagesAndLabels('training','./data/')
 #imagesAndLabels = list(trainingData.getImagesAndLabels())
 #labels = list(trainingData.getLabels())
-node6 = trainingData.getInputNodes(3)
-label6 = trainingData.getLabel(3)
+#node6 = trainingData.getInputNodes(3)
+#label6 = trainingData.getLabel(3)
 
-print("nodes size ",node6.size,"node[784] ",node6[784])
-print("label ",label6)
-print("Number of sets ",trainingData.getNumSets())
-print("Input dimension = ",trainingData.getInputDimension(),"Output dimension = ",trainingData.getOutputDimension())
+#print("nodes size ",node6.size,"node[784] ",node6[784])
+#print("label ",label6)
+#print("Number of sets ",trainingData.getNumSets())
+#print("Input dimension = ",trainingData.getInputDimension(),"Output dimension = ",trainingData.getOutputDimension())
 
-
-print("")
 print("")
 print("Set up weights structure")
-testStructure = CNNStructure('training')
+#testStructure = CNNStructure('training')
 #tempList  = [trainingData.getInputDimension(),16,16,trainingData.getOutputDimension()]
-tempList = [5,4,3,2]
+tempList = [5,3,3,2]
+tempInput = np.array([0 for col in range(5)],dtype=float).reshape(5,1)
+tempDesired = np.array([0 for col in range(2)],dtype=float).reshape(2,1)
+tempDesired[0] = 1.;
 testStructure2 = CNNStructure(tempList,.5,.5)
+for x in range(5):
+    tempInput[x] = x
+tempInput[4] = 1.
+print("tempInput")
+print(tempInput)
+#tempInput = np.array[1.,2.,3.,4.,5.]
+
+testStructure2.updateLayerNodes(tempInput)
+
+tempGradHoldStructure = CNNStructure(tempList)
+print("tempGradHoldStructure",tempGradHoldStructure.weights[0], tempGradHoldStructure.weights[1])
+testStructure2.makeGradPass(tempGradHoldStructure,tempDesired)
 #trainingData.displayImage(3)
 #trainingData.asciiShow(3)
+for layer in range(len(tempList)-1):
+    print("Layer = ",layer)
+    print(testStructure2.weights[layer])
+
+print("Cost = ",testStructure2.calcCost(tempInput,tempDesired))
 
